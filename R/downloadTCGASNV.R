017eccbaa10c4e85df1ce3023c9465782e1b2ff3
Marc Williams <hfx042@frontend11.apocrita>
2019-08-20 11:56:22 +0100

    Seperate downloading of CNA and SNV

--- R/downloadTCGASNV.R
+++ R/downloadTCGASNV.R
@@ -7,10 +7,6 @@ library(argparse)
 parser <- ArgumentParser(description = "Generate Final Figures")
 parser$add_argument('--MAFfile', type='character',
                     help="TCGA MAF file hg38")
-parser$add_argument('--CNVfile', type='character',
-                    help="TCGA CNV file hg38")
-parser$add_argument('--ascatcellularity', type='character',
-                    help="Ascat cellularity file")
 args <- parser$parse_args()
 
 #get all TCGA cancer subtype codes
@@ -46,15 +42,3 @@ dfhg38 %>%
     distinct(sampleid, cancertype) %>%
     group_by(cancertype) %>%
     summarise(n = n())
-
-message("Download copy number data...")
-tcgacodes <- TCGAbiolinks:::getGDCprojects()$project_id
-query <- GDCquery(project = tcgacodes[str_detect(tcgacodes, "TCGA")],
-                data.category = "Copy Number Variation",
-                data.type = "Copy Number Segment")
-
-cnvsamples <- getResults(query)
-GDCdownload(query)
-dfhg38cnv <- GDCprepare(query)
-message("Writing CNA to file...")
-write_delim(dfhg38cnv, args$CNVfile, delim = ",")
